de2b44e0e7538cec1bcf41f4d71b38ec60a07e01
zhangjiahao <961099916@qq.com>
2020-04-11 13:04:43 +0800

    'update'

--- push.sh
+++ push.sh
@@ -5,4 +5,4 @@ git add .
 
 git commit -m 'update'
 
-git push -f https://github.com/961099916/961099916.github.ioi.git master:docst
+git push -f https://github.com/961099916/961099916.github.io.git master:docs
