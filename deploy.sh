0f3d8c100ea4b2c44396a09c6533e972b19354f5
zhangjiahao <961099916@qq.com>
2020-05-13 16:00:06 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:961099916/961099916.github.io.git master:master
+git push -f https://github.com/961099916/961099916.github.io.git master:master
 
 cd ..
 rm -rf ./dist
