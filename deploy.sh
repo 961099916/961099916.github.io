067c18efdb2f74975bd95182098cd1cce5fb0cef
zhangjiahao <961099916@qq.com>
2021-07-09 22:53:52 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #! /bin/bash
-
-
 # 生成静态文件
- vuepress build docs
-
+vuepress build docs && cp CNAME ./dist/ && git init  && git add -A && git commit -m 'deploy'
 # 进入生成的文件夹
- cp CNAME ./dist/
+cp CNAME ./dist/
 cd ./dist
- git init
- git add -A
- git commit -m 'deploy'
-
+git init
+git add -A
+git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f https://github.com/961099916/961099916.github.io.git master:master
-
+git push -f https://github.com/961099916/961099916.github.io.git master:master
 cd ..
- rm -rf ./dist
+rm -rf ./dist
