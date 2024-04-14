b9ba27dc59fa2e8207a242f1005cc3c91b4697c3
zhangjiahao <961099916@qq.com>
2024-04-14 18:26:47 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+yarn build
+
+cd .vuepress/dist
+
+ls
+
+git init
+git add -A
+git commit -m 'docs: update docs'
+
+# git push -f git@github.com:recoluan/vuepress-reco-doc.git master
+
+# git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
+
+git remote remove origin
+
+git remote add origin git@github.com:961099916/961099916.github.io.git
+
+git remote -v
+
+git push origin master -f
+
+cd ../
+cd ../
+./push.sh
