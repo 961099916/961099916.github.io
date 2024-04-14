b706caae166e2ca44018b4d6f32c42b4c67416c7
张家豪 <961099916@qq.com>
2024-04-14 21:02:11 +0800

    push

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -72,7 +72,7 @@ export default hopeTheme({
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     comment: {
       provider: "Waline",
-      serverURL: "vercel-nine-henna-21.vercel.app",
+      serverURL: "https://vercel-l4zyv5epd-961099916s-projects.vercel.app/",
     },
     components: {
       components: ["Badge", "VPCard"],
