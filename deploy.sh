5b9c59ebba1da5665a252d3479e55dd18a103ee9
张家豪 <961099916@qq.com>
2024-04-14 20:30:01 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
-npm run build
+# npm run build
 
-cd src/.vuepress/dist
+# cd src/.vuepress/dist
 
-ls
+# ls
 
-git init
-git add -A
-git commit -m 'docs: update docs'
+# git init
+# git add -A
+# git commit -m 'docs: update docs'
 
-# git push -f git@github.com:recoluan/vuepress-reco-doc.git master
+# # git push -f git@github.com:recoluan/vuepress-reco-doc.git master
 
-# git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
+# # git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
 
-git remote remove origin
+# git remote remove origin
 
-git remote add origin git@github.com:961099916/961099916.github.io.git
+# git remote add origin git@github.com:961099916/961099916.github.io.git
 
-git remote -v
+# git remote -v
 
-git push origin master -f
+# git push origin master -f
+#
 
-
-cd ../
-cd ../
-cd ../
+# cd ../
+# cd ../
+# cd ../
 ./push.sh
